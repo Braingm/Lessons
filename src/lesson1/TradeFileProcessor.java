707a40842cbd1eccb49a804632344adfc999c9d9
braingm-laptop <valera.kineshov@mail.ru>
2025-04-13 19:00:01 +0300

    Version 1.2.250413

---
+++ src/lesson1/TradeFileProcessor.java
@@ -0,0 +1,13 @@
+package src.lesson1;
+
+public class TradeFileProcessor {
+    private final TradeManager tradeManager;
+
+    public TradeFileProcessor(TradeManager tradeManager){
+        this.tradeManager = tradeManager;
+    }
+
+    public void processTradeFiles(String inputFile, String outputFile){
+
+    }
+}
