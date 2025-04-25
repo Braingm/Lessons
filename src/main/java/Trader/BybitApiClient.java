bab06f6f6a06c100173c8eed5491c162c6b673a6
Braingm <Braingm@mail.ru>
2025-04-26 01:04:50 +0300

    Version 1.5.250426 Подключение BYBIT API через REST

--- src/main/java/Trader/BybitApiClient.java
+++ src/main/java/Trader/BybitApiClient.java
@@ -4,14 +4,15 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
+import okhttp3.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.nio.charset.StandardCharsets;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
@@ -64,6 +65,44 @@ public class BybitApiClient {
             String priceStr = priceResponse.getResult().getList()[0].getLastPrice();
             logger.info("Current price for {}: {}", symbol, priceStr);
             return new BigDecimal(priceStr);
+        }
+    }
+
+    public String placeMarketOrder(String symbol, String side, BigDecimal qty) throws IOException {
+        String endpoint = "/v5/order/create";
+        long timestamp = System.currentTimeMillis();
+        OrderRequest order = new OrderRequest("spot", symbol, side, "Market", qty.toString());
+
+        String jsonBody = mapper.writeValueAsString(order);
+        String signStr = timestamp + apiKey + RECV_WINDOW + jsonBody;
+        String signature = hmacSha256(signStr, apiSecret);
+
+        Request request = new Request.Builder()
+                .url(baseUrl + endpoint)
+                .post(RequestBody.create(jsonBody, MediaType.get("application/json")))
+                .addHeader("X-BAPI-API-KEY", apiKey)
+                .addHeader("X-BAPI-TIMESTAMP", String.valueOf(timestamp))
+                .addHeader("X-BAPI-RECV-WINDOW", RECV_WINDOW)
+                .addHeader("X-BAPI-SIGN", signature)
+                .build();
+    }
+
+    private String hmacSha256(String data, String key) throws  RuntimeException{
+        try {
+            Mac mac = Mac.getInstance("HmacSHA256");
+            SecretKeySpec keySpec = new SecretKeySpec(key.getBytes(StandardCharsets.UTF_8),"HmacSHA256");
+            mac.init(keySpec);
+            byte[] hash = mac.doFinal(data.getBytes(StandardCharsets.UTF_8));
+            StringBuilder hexString = new StringBuilder();
+            for (byte b : hash){
+                String hex = Integer.toHexString(0xff & b);
+                if (hex.length() == 1) hexString.append('0');
+                hexString.append(hex);
+            }
+            return hexString.toString();
+        } catch (Exception e) {
+            logger.error("Failed to compute sign", e);
+            throw new RuntimeException("Signature error",e);
         }
     }
 
